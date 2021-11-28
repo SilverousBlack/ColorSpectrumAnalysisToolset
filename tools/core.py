8e3f31bf7068bdf16e858e6dc74e093ae6b3d99e
SilverousBlack <juliancalebsegundo@gmail.com>
2021-11-29 03:46:20 +0800

    4.4.0
    setup of multiple color mode support

---
+++ tools/core.py
@@ -0,0 +1,13 @@
+# Core.py contains core functionalities and imports for entire toolset
+
+# Supported Image Formats
+__meta_supported_formats__ = ("PNG", "JPEG", "JPEG 2000")
+
+def getmeta_supp_formats():
+    return __meta_supported_formats__
+
+# Supported Color Modes
+__meta_supported_modes__ = ("RGB", "RGBA")
+
+def getmeta_supp_modes():
+    return __meta_supported_modes__
